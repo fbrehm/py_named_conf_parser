ceb0237eaf32d41263f96f8ca2109c59c25d1574
Frank Brehm <frank.brehm@pixelpark.com>
2020-06-08 16:28:53 +0200

    Refactoring test/test_parser.py

--- test/test_parser.py
+++ test/test_parser.py
@@ -3,39 +3,28 @@
 '''
 @author: Frank Brehm
 @contact: frank.brehm@profitbricks.com
-@organization: Profitbricks GmbH
-@copyright: © 2010 - 2013 by Profitbricks GmbH
+@copyright: © 2020 Frank Brehm, Berlin
 @license: GPL3
 @summary: test script (and module) for unit tests on named.conf parser object
 '''
 
-import unittest
 import os
 import sys
 import logging
 
-libdir = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), '..', 'src'))
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+libdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lib'))
 sys.path.insert(0, libdir)
 
-import general
 from general import NamedConfParseTestcase, get_arg_verbose, init_root_logger
 
-import pb_base.object
-from pb_base.object import PbBaseObjectError
+from fb_tools.common import pp, to_bool
 
-from pb_base.common import pp
-
-import named_conf
-from named_conf import CommonNamedConfError
-
-import named_conf.parser
-from named_conf.parser import NamedConfParserError
-from named_conf.parser import NamedConf
-from named_conf.parser import NamedConfParser
-from named_conf.parser import NamedConfEntry
-from named_conf.parser import NamedConfBlock
-
-log = logging.getLogger(__name__)
+LOG = logging.getLogger('test_parser')
 
 #==============================================================================
 
@@ -45,90 +34,105 @@ class TestNamedConfParser(NamedConfParseTestcase):
     def setUp(self):
         pass
 
-    #--------------------------------------------------------------------------
-    def test_parser_object(self):
+    # -------------------------------------------------------------------------
+    def tearDown(self):
+        pass
 
-        log.info("Testing init of a parser object.")
+    # -------------------------------------------------------------------------
+    def test_import(self):
 
-        cur_dir = os.path.dirname(sys.argv[0])
-        curdir_real = os.path.realpath(cur_dir)
+        LOG.info("Testing import of named_conf ...")
+        import named_conf                                           # noqa
 
-        log.debug("Testing init object without any arguments ...")
-        parser = NamedConfParser(verbose = self.verbose)
-        if self.verbose > 2:
-            log.debug("Parser object:\n%s", parser)
-        self.assertEqual(parser.chroot, None)
+        if self.verbose:
+            LOG.debug("Version of Module named_conf: {!r}".format(named_conf.__version__))
+            LOG.debug("Default named.conf: {!r}".format(named_conf.DEFAULT_NAMED_CONF))
+            LOG.debug("Default Bind directory: {!r}".format(named_conf.DEFAULT_BIND_DIR))
 
-        log.debug("Testing init object with the test directory as a chroot dir.")
-        parser = NamedConfParser(chroot = cur_dir, verbose = self.verbose)
-        if self.verbose > 2:
-            log.debug("Parser object:\n%s", parser)
-        self.assertEqual(parser.chroot, curdir_real)
-
-    #--------------------------------------------------------------------------
-    def test_parsing_simple(self):
-
-        log.info("Testing parsing of a simple named.conf ...")
-
-        cur_dir = os.path.dirname(sys.argv[0])
-        curdir_real = os.path.realpath(cur_dir)
-        chroot = os.path.join(curdir_real, 'single')
-
-        parser = NamedConfParser(chroot = chroot, verbose = self.verbose)
-        if self.verbose > 2:
-            log.debug("Parser object:\n%s", parser)
-
-        named_conf = parser()
-        if self.verbose > 2:
-            log.debug("NamedConf object:\n%s", pp(
-                    named_conf.as_dict(short = True)))
-        self.assertIsInstance(named_conf, NamedConf)
-
-    #--------------------------------------------------------------------------
-    def test_named_conf_entry(self):
-
-        log.info("Testing creating and manipulating a NamedConfEntry.")
-
-        entry = NamedConfEntry(verbose = self.verbose)
-        if self.verbose > 2:
-            log.debug("NamedConfEntry object:\n%s", pp(entry.as_dict(short = True)))
-
-        entry.append('Hallo')
-        entry.append(33)
-        entry.append('bunnies')
-        if self.verbose > 2:
-            log.debug("NamedConfEntry object:\n%s", pp(entry.as_dict(short = True)))
-        log.debug("NamedConfEntry typecast into str: %r", str(entry))
-        self.assertEqual(str(entry), 'Hallo 33 bunnies;')
-        log.debug("NamedConfEntry repr: %r", entry)
-
-        #entry.indent = 1
-        #log.debug("NamedConfEntry typecast into str with indention: %r", str(entry))
-        #self.assertEqual(str(entry), '    Hallo 33 bunnies;')
-
-    #--------------------------------------------------------------------------
-    def test_named_conf_block(self):
-
-        log.info("Testing creating and manipulating a NamedConfBlock.")
-
-        block = NamedConfBlock(verbose = self.verbose)
-        if self.verbose > 2:
-            log.debug("NamedConfBlock object:\n%s", pp(block.as_dict(short = True)))
-        log.debug("NamedConfBlock typecast into str: %r", str(block))
-        self.assertEqual(str(block), '{ }')
-
-        block.append(NamedConfEntry('10.1.1.1', verbose = self.verbose))
-        block.append(NamedConfEntry('private_ips', verbose = self.verbose))
-
-        if self.verbose > 2:
-            log.debug("NamedConfBlock object:\n%s", pp(block.as_dict(short = True)))
-        log.debug("NamedConfBlock typecast into str: %r", str(block))
-        self.assertEqual(str(block), '{\n    10.1.1.1;\n    private_ips;\n}')
-        log.debug("NamedConfBlock repr: %r", block)
-
-        block.indent = 2
-        log.debug("NamedConfBlock typecast into str with indention: %r", str(block))
-        self.assertEqual(str(block), '{\n            10.1.1.1;\n            private_ips;\n        }')
+#    #--------------------------------------------------------------------------
+#    def test_parser_object(self):
+#
+#        log.info("Testing init of a parser object.")
+#
+#        cur_dir = os.path.dirname(sys.argv[0])
+#        curdir_real = os.path.realpath(cur_dir)
+#
+#        log.debug("Testing init object without any arguments ...")
+#        parser = NamedConfParser(verbose = self.verbose)
+#        if self.verbose > 2:
+#            log.debug("Parser object:\n%s", parser)
+#        self.assertEqual(parser.chroot, None)
+#
+#        log.debug("Testing init object with the test directory as a chroot dir.")
+#        parser = NamedConfParser(chroot = cur_dir, verbose = self.verbose)
+#        if self.verbose > 2:
+#            log.debug("Parser object:\n%s", parser)
+#        self.assertEqual(parser.chroot, curdir_real)
+#
+#    #--------------------------------------------------------------------------
+#    def test_parsing_simple(self):
+#
+#        log.info("Testing parsing of a simple named.conf ...")
+#
+#        cur_dir = os.path.dirname(sys.argv[0])
+#        curdir_real = os.path.realpath(cur_dir)
+#        chroot = os.path.join(curdir_real, 'single')
+#
+#        parser = NamedConfParser(chroot = chroot, verbose = self.verbose)
+#        if self.verbose > 2:
+#            log.debug("Parser object:\n%s", parser)
+#
+#        named_conf = parser()
+#        if self.verbose > 2:
+#            log.debug("NamedConf object:\n%s", pp(
+#                    named_conf.as_dict(short = True)))
+#        self.assertIsInstance(named_conf, NamedConf)
+#
+#    #--------------------------------------------------------------------------
+#    def test_named_conf_entry(self):
+#
+#        log.info("Testing creating and manipulating a NamedConfEntry.")
+#
+#        entry = NamedConfEntry(verbose = self.verbose)
+#        if self.verbose > 2:
+#            log.debug("NamedConfEntry object:\n%s", pp(entry.as_dict(short = True)))
+#
+#        entry.append('Hallo')
+#        entry.append(33)
+#        entry.append('bunnies')
+#        if self.verbose > 2:
+#            log.debug("NamedConfEntry object:\n%s", pp(entry.as_dict(short = True)))
+#        log.debug("NamedConfEntry typecast into str: %r", str(entry))
+#        self.assertEqual(str(entry), 'Hallo 33 bunnies;')
+#        log.debug("NamedConfEntry repr: %r", entry)
+#
+#        #entry.indent = 1
+#        #log.debug("NamedConfEntry typecast into str with indention: %r", str(entry))
+#        #self.assertEqual(str(entry), '    Hallo 33 bunnies;')
+#
+#    #--------------------------------------------------------------------------
+#    def test_named_conf_block(self):
+#
+#        log.info("Testing creating and manipulating a NamedConfBlock.")
+#
+#        block = NamedConfBlock(verbose = self.verbose)
+#        if self.verbose > 2:
+#            log.debug("NamedConfBlock object:\n%s", pp(block.as_dict(short = True)))
+#        log.debug("NamedConfBlock typecast into str: %r", str(block))
+#        self.assertEqual(str(block), '{ }')
+#
+#        block.append(NamedConfEntry('10.1.1.1', verbose = self.verbose))
+#        block.append(NamedConfEntry('private_ips', verbose = self.verbose))
+#
+#        if self.verbose > 2:
+#            log.debug("NamedConfBlock object:\n%s", pp(block.as_dict(short = True)))
+#        log.debug("NamedConfBlock typecast into str: %r", str(block))
+#        self.assertEqual(str(block), '{\n    10.1.1.1;\n    private_ips;\n}')
+#        log.debug("NamedConfBlock repr: %r", block)
+#
+#        block.indent = 2
+#        log.debug("NamedConfBlock typecast into str with indention: %r", str(block))
+#        self.assertEqual(str(block), '{\n            10.1.1.1;\n            private_ips;\n        }')
 
 #==============================================================================
 
@@ -140,14 +144,15 @@ if __name__ == '__main__':
         verbose = 0
     init_root_logger(verbose)
 
-    log.info("Starting tests ...")
+    LOG.info("Starting tests ...")
 
     suite = unittest.TestSuite()
 
-    suite.addTest(TestNamedConfParser('test_parser_object', verbose))
-    suite.addTest(TestNamedConfParser('test_parsing_simple', verbose))
-    suite.addTest(TestNamedConfParser('test_named_conf_entry', verbose))
-    suite.addTest(TestNamedConfParser('test_named_conf_block', verbose))
+    suite.addTest(TestNamedConfParser('test_import', verbose))
+    # suite.addTest(TestNamedConfParser('test_parser_object', verbose))
+    # suite.addTest(TestNamedConfParser('test_parsing_simple', verbose))
+    # suite.addTest(TestNamedConfParser('test_named_conf_entry', verbose))
+    # suite.addTest(TestNamedConfParser('test_named_conf_block', verbose))
 
     runner = unittest.TextTestRunner(verbosity = verbose)
 
